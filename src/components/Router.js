5771a56409fee2872a88213a182a5037faca354f
shinpanda <duldugo1@gmail.com>
2021-10-22 00:42:31 +0900

    7장까지

---
+++ src/components/Router.js
@@ -0,0 +1,31 @@
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
+import Auth from "routes/Auth";
+import Home from "routes/Home";
+import Profile from "routes/Profile";
+import Navigation from "./Navigation";
+
+const AppRouter = ({ isLoggedIn, userObj, refreshUser }) => {
+  return (
+    <Router>
+      {isLoggedIn && <Navigation userObj={userObj} />}
+      <Switch>
+        {isLoggedIn ? (
+          <>
+            <Route exact path="/">
+              <Home userObj={userObj} />
+            </Route>
+            <Route exact path="/profile">
+              <Profile refreshUser={refreshUser} userObj={userObj} />
+            </Route>
+          </>
+        ) : (
+          <Route exact path="/">
+            <Auth />
+          </Route>
+        )}
+      </Switch>
+    </Router>
+  );
+};
+
+export default AppRouter;
---
+++ src/routes/Profile.js
@@ -0,0 +1,45 @@
+import { authService, dbService } from "fbase";
+import { useState, useEffect } from "react";
+import { useHistory } from "react-router-dom";
+
+const Profile = ({ userObj, refreshUser }) => {
+  const history = useHistory();
+  const [newDisplayName, setNewDisplayName] = useState(userObj.displayName);
+
+  const onLogOutClick = () => {
+    authService.signOut();
+    history.push("/");
+  };
+
+  const onChange = (event) => {
+    const {
+      target: { value },
+    } = event;
+    setNewDisplayName(value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (userObj.displayName !== newDisplayName) {
+      await userObj.updateProfile({ displayName: newDisplayName });
+      refreshUser();
+    }
+  };
+
+  return (
+    <>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          placeholder="Display name"
+          onChange={onChange}
+          value={newDisplayName}
+        />
+        <input type="submit" value="Update Profile" />
+      </form>
+      <button onClick={onLogOutClick}>Log Out</button>
+    </>
+  );
+};
+
+export default Profile;
